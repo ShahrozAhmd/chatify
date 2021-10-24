68e77fd388aec4347e65b93b687c705a9ff4fef5
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-10-24 12:37:27 +0500

    added firebase google auth demo

--- src/Api/firebase.js
+++ src/Api/firebase.js
@@ -1,4 +1,6 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,3 +13,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+// const _signInWithGoogle = signInWithPopup(auth, provider)
+
+// export {_signInWithGoogle}
+export{provider,auth}
--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
-import React, { useState, useReducer, useEffect,createContext } from "react";
+import React, { useState, useReducer, useEffect, createContext } from "react";
 import FirstComponent from "./components/first-component";
+import { provider,auth} from "./Api/firebase";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 function App() {
   // const [count, setCount] = useState(0);
   // const [currentState, dispatch] = useReducer(()=>setCount(item=>item+1),count);
 
-  
-  
   return (
-   <FirstComponent/>
+    <>
+      <button
+        onClick={() => {
+          signInWithPopup(auth,provider).then(res=>console.log(res))
+        }}
+      >
+        Click
+      </button>
+      <FirstComponent />
+    </>
   );
 }
 
