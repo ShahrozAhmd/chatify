7ab55332e35efab3f3d4309ce69b5823719edce4
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-11-07 21:57:59 +0500

    changes

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -9,7 +9,7 @@ const  Routes = () => {
     return (
         <>
         <Switch>
-            <Route exact path='/' component={Auth}/>
+            <Route  path='/' component={Auth}/>
             <Route exact path='/chat' component={Chat}/>
         </Switch>
             
